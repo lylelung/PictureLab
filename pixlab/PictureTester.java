c43fb43f76d7852090a65fef33c222f04bad5793
lylelung <43968529+lylelung@users.noreply.github.com>
2019-03-11 11:49:31 -0700

    basically noithing

--- pixlab/PictureTester.java
+++ pixlab/PictureTester.java
@@ -71,7 +71,8 @@ public class PictureTester
     //testNegate();
     //testGrayscale();
     //testFixUnderwater();
-    //testMirrorVertical();
+ 
+    testMirrorVertical();
     //testMirrorTemple();
     //testMirrorArms();
     //testMirrorGull();
